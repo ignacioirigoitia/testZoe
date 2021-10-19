0ebdcd5ce12218db66966b683c7e943c55f6840b
Ignacio Irigoitia <ignacio.irigoitia@gmail.com>
2021-10-19 16:06:24 -0300

    listo prueba en ocean

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "e49714637330edbc7084564265c1eb82",
+  "assets/AssetManifest.json": "e44a9dcc9cfd01135c7a0d5a401621c4",
 "assets/assets/buscador.jpg": "72fc53dc3d3aa4906a9913eb504cad71",
 "assets/assets/green-fox.png": "b56803f5afc26226e3406b32e6721115",
 "assets/assets/iconGoogle.png": "ecc844337bfe607020e2aa4aa3562e29",
+"assets/assets/loader-git.gif.gif": "06c71a5dc496577867dba77115cff321",
 "assets/assets/logo.jpg": "17c395f362d70b92b26bc4cce9929f75",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
@@ -17,9 +18,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "910d87c31f9d01f747ba9cd2524fd7b5",
-"/": "910d87c31f9d01f747ba9cd2524fd7b5",
-"main.dart.js": "c07bdcfc99af6393a87be5462163a655",
+"index.html": "775b5a87c5a5836a205112fc3a7d4779",
+"/": "775b5a87c5a5836a205112fc3a7d4779",
+"main.dart.js": "6fc2d8bf86d9c01ce848aafdb7dadc00",
 "manifest.json": "91b3b6356f32947e37135f8cae5f58c1",
 "version.json": "adec0feee51ebd45457c63df08c414b0"
 };
